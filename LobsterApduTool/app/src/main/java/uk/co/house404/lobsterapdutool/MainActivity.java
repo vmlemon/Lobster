31d4b5b39e57c4c55df62ce3ae7a6d442529fce5
Tyson Key <tyson.key@gmail.com>
2016-09-24 15:09:36 +0100

    Create the project

---
+++ LobsterApduTool/app/src/main/java/uk/co/house404/lobsterapdutool/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.co.house404.lobsterapdutool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
