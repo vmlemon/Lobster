d948bafc3de08a2788c8c9906e4e9828f1a8c92b
Tyson Key <tyson.key@gmail.com>
2016-09-24 15:25:52 +0100

    XML, and APDUs

--- LobsterApduTool/app/src/main/java/uk/co/house404/lobsterapdutool/Crustacean.java
+++ LobsterApduTool/app/src/main/java/uk/co/house404/lobsterapdutool/Crustacean.java
@@ -2,15 +2,22 @@ package uk.co.house404.lobsterapdutool;
 
 import android.app.Service;
 import android.content.Intent;
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
 import android.os.IBinder;
 
-public class Crustacean extends Service {
+
+public class Crustacean extends HostApduService {
     public Crustacean() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
+        System.out.println("Crustacean APDU :\r\n " + apdu
+            + "Bundle : \r\n" + extras + "\r\n");
+    }
+    @Override
+    public void onDeactivated(int reason) {
+        System.out.println("Deactivated for " + reason + "\r\n")
     }
 }
